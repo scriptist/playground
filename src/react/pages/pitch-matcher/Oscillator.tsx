58fcb07a34665045e9550ad5c33ad11da89c828c
Mike Berman <mike@berman.xyz>
2024-01-14 10:52:57 +0000

    Basic pitch matcher

--- src/react/pages/pitch-matcher/Oscillator.tsx
+++ src/react/pages/pitch-matcher/Oscillator.tsx
@@ -1,30 +1,41 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
-const Oscillator: React.FC<{ isActive: boolean; pitchHz: number }> = ({
-  pitchHz,
-  isActive,
-}) => {
-  const [audioCtx, setAudioCtx] = useState<AudioContext | null>();
-
-  useEffect(() => {
-    setAudioCtx(new AudioContext());
-  }, []);
+const Oscillator: React.FC<{
+  isActive: boolean;
+  pitchHz: number;
+  type: OscillatorType;
+}> = ({ pitchHz, isActive, type }) => {
+  const [audioCtx] = useState<AudioContext>(() => new AudioContext());
+  const [oscillator, setOscillator] = useState<OscillatorNode | null>();
 
   useEffect(() => {
-    if (!isActive || audioCtx == null) {
+    if (!isActive) {
       return;
     }
 
-    // create Oscillator node
-    const oscillator = audioCtx.createOscillator();
+    const volume = audioCtx.createGain();
+    volume.connect(audioCtx.destination);
+    volume.gain.value = type === "sawtooth" ? 0.3 : 1;
 
-    oscillator.type = "sine";
-    oscillator.frequency.setValueAtTime(pitchHz, audioCtx.currentTime); // value in hertz
-    oscillator.connect(audioCtx.destination);
+    const oscillator = audioCtx.createOscillator();
+    oscillator.type = type;
+    oscillator.connect(volume);
     oscillator.start();
 
-    return () => oscillator.stop();
-  }, [audioCtx, pitchHz, isActive]);
+    setOscillator(oscillator);
+
+    return () => {
+      oscillator.stop();
+      setOscillator(null);
+    };
+  }, [isActive, type]);
+
+  useEffect(() => {
+    oscillator?.frequency.setValueAtTime(
+      pitchHz,
+      oscillator.context.currentTime
+    );
+  }, [oscillator, pitchHz]);
 
   return null;
 };
--- src/react/pages/pitch-matcher/PitchMatcher.tsx
+++ src/react/pages/pitch-matcher/PitchMatcher.tsx
@@ -1,13 +1,104 @@
-import React from "react";
+import React, { useState } from "react";
 import Oscillator from "./Oscillator";
+import randomItem from "random-item";
 
 const PitchMatcher: React.FC = () => {
+  const [isActive, setIsActive] = useState(false);
+  const [userPitch, setUserPitch] = useState(getFrequencyOfNoteInHz("A", 4));
+  const [isUserSliding, setIsUserSliding] = useState(false);
+  const [targetPitch, setTargetPitch] = useState(() => getRandomFrequency());
+  const [showResult, setShowResult] = useState(true);
+
   return (
     <div>
-      Pitch Matcher
-      <Oscillator pitchHz={440} isActive={false} />
+      <h1>Pitch Matcher</h1>
+      <label>
+        <input
+          type="checkbox"
+          checked={isActive}
+          onChange={(e) => setIsActive(e.target.checked)}
+        />
+        Active
+      </label>
+      <br />
+      <input
+        max={getFrequencyOfNoteInHz(
+          notes[notes.length - 1],
+          octaves[octaves.length - 1]
+        )}
+        min={getFrequencyOfNoteInHz(notes[0], octaves[0])}
+        onChange={(e) => setUserPitch(parseFloat(e.target.value))}
+        onPointerDown={() => {
+          setIsUserSliding(true);
+          setIsActive(true);
+        }}
+        onPointerUp={() => setIsUserSliding(false)}
+        style={{ width: "500px" }}
+        type="range"
+        value={userPitch}
+      />
+      <Oscillator
+        pitchHz={userPitch}
+        isActive={isActive && isUserSliding}
+        type="sawtooth"
+      />
+      <Oscillator
+        pitchHz={targetPitch}
+        isActive={isActive && !isUserSliding}
+        type="sine"
+      />
+      <br />
+      {showResult ? (
+        <>
+          <button
+            onClick={() => {
+              setShowResult(false);
+              setIsActive(true);
+              setTargetPitch(getRandomFrequency());
+            }}
+          >
+            Reset
+          </button>
+          <br />
+          <h2>
+            Score:{" "}
+            {Math.max(
+              0,
+              100 - Math.round(Math.abs(userPitch / targetPitch - 1) * 500)
+            )}
+          </h2>
+        </>
+      ) : (
+        <button
+          onClick={() => {
+            setShowResult(true);
+            setIsActive(false);
+          }}
+        >
+          Submit
+        </button>
+      )}
     </div>
   );
 };
 
 export default PitchMatcher;
+
+const notes = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"];
+const octaves = [4, 5];
+
+type NoteType = (typeof notes)[number];
+
+function getRandomFrequency(): number {
+  const note = randomItem(notes);
+  const octave = randomItem(octaves);
+
+  return getFrequencyOfNoteInHz(note, octave);
+}
+
+const getNoteDistanceFromC0 = (note: NoteType, octave: number) => {
+  return notes.indexOf(note) + octave * 12;
+};
+
+const getFrequencyOfNoteInHz = (note: NoteType, octave: number) =>
+  440 * Math.pow(2, (getNoteDistanceFromC0(note, octave) - 57) / 12);
